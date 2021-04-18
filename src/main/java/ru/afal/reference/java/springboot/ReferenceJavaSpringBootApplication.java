61bfbd2ec3525a0c53916238eb21cc6ddf21f13f
afal007 <mYc#0eB6XC8V>
2021-04-18 14:35:57 +0700

    Initializing the project

---
+++ src/main/java/ru/afal/reference/java/springboot/ReferenceJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.afal.reference.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferenceJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/afal/reference/java/springboot/ReferenceJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.afal.reference.java.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
